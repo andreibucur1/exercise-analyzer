625a52ab8278c967f1b52e5d9a82210b8ff80f38
andreibucur1 <andreibucur051@gmail.com>
2025-07-22 15:47:08 +0300

    Added tricep extension and lateral raise

--- AnalyzerClass.py
+++ AnalyzerClass.py
@@ -12,8 +12,10 @@ class ExerciseAnalyzer():
         self.detector = PostureDetector()
     def analyzeVideo(self, video_path, exercise):
         cap = cv2.VideoCapture(video_path)
-        l_angles=[]
-        r_angles=[]
+        l_arm_angles=[]
+        r_arm_angles=[]
+        l_shoulder_angles=[]
+        r_shoulder_angles=[]
         while True:
             succes, img = cap.read()
             if not succes:
@@ -22,18 +24,27 @@ class ExerciseAnalyzer():
             img = self.detector.findPose(img, draw=True)
             self.lmlist = self.detector.getPosition(img, draw=False)
             if len(self.lmlist) != 0:
-                l_angles.append(self.getLeftArm())
-                r_angles.append(self.getRightArm())
+                l_arm_angles.append(self.getLeftArm())
+                r_arm_angles.append(self.getRightArm())
+                l_shoulder_angles.append(self.getLeftShoulder())
+                r_shoulder_angles.append(self.getRightShoulder())
 
 
             cv2.imshow("Image", img)
             cv2.waitKey(1)
         
-        mark = 0
-        message = ""
+        
+        match exercise:
+            case "bicepCurl":
+                mark, message = self.checkBicepCurl(l_arm_angles, r_arm_angles)
+            case "tricepExtension":
+                mark, message = self.checkTricepExtension(l_arm_angles, r_arm_angles)
+            case "lateralRaise":
+                mark, message = self.checkLateralRaises(l_shoulder_angles, r_shoulder_angles)
+            case _:
+                mark, message = 0, "Unknown exercise"
+        
 
-        if exercise == "bicepCurl":
-            mark, message = self.checkBicepCurl(l_angles, r_angles)
         return mark, message
 
     def findAngle(self, a, b, c):
@@ -137,8 +148,168 @@ class ExerciseAnalyzer():
         
         return mark, message
 
+    def checkTricepExtension(self, left_arm_angles, right_arm_angles):
         
+    
+        
+        filtered_left_angles = savgol_filter(left_arm_angles, window_length=20, polyorder=2, mode= "nearest")
+        filtered_right_angles = savgol_filter(right_arm_angles, window_length=20, polyorder=2,  mode= "nearest")
 
+        max_left_id = find_peaks(filtered_left_angles)[0]
+        max_right_id = find_peaks(filtered_right_angles)[0]
+
+        min_left_id = find_peaks(-filtered_left_angles)[0]
+        min_right_id = find_peaks(-filtered_right_angles)[0]
+
+
+        # plt.figure(figsize=(12, 6))
+        # plt.plot(left_arm_angles, label='Unghi Dreapta Brut', alpha=0.7, color = 'blue')
+        # plt.plot(filtered_left_angles, label='Unghi Dreapta Prelucrat', alpha=0.7, color = 'red')
+        # # plt.plot(right_arm_angles, label='Unghi Dreapta Brut', alpha=0.7, color = 'blue')
+        # # plt.plot(filtered_right_angles, label='Unghi Dreapta Prelucrat', alpha=0.7, color = 'red')
+        # plt.show()
+
+
+        not_low_enough_l = 0
+        not_high_enough_l = 0
+
+        not_low_enough_r = 0
+        not_high_enough_r = 0
+
+        mark_l = 0
+        mark_r = 0
+
+
+
+        if len(min_left_id) < len(max_left_id):
+            rep_capture_error = 1
+            index_number_l = len(min_left_id)
+        else:
+            rep_capture_error = 0
+            index_number_l = len(max_left_id)
+
+        for i in range(index_number_l):
+            if filtered_left_angles[max_left_id[i + rep_capture_error]] < 165:
+                not_low_enough_l += 1
+            if filtered_left_angles[min_left_id[i]] > 90:
+                not_high_enough_l += 1
+
+        number_of_mistakes_l = not_high_enough_l + not_low_enough_l
+        mark_l = 10 - 5 * number_of_mistakes_l / index_number_l
+
+
+
+
+
+        if len(min_right_id) < len(max_right_id):
+            rep_capture_error = 1
+            index_number_r = len(min_right_id)
+        else:
+            rep_capture_error = 0
+            index_number_r = len(max_right_id)
+
+        for i in range(index_number_r):
+            if filtered_right_angles[max_right_id[i + rep_capture_error]] < 165:
+                not_low_enough_r += 1
+            if filtered_right_angles[min_right_id[i ]] > 70:
+                not_high_enough_r += 1
+        number_of_mistakes_r = not_high_enough_r + not_low_enough_r
+        mark_r = 10 - 5 * number_of_mistakes_r / index_number_r
+
+
+        mark = (mark_r+mark_l)/2
+        
+        if not_low_enough_l == 0 and not_high_enough_l == 0 and not_low_enough_r == 0 and not_high_enough_r == 0:
+            message = "Perfect form!"
+        elif (not_low_enough_l != 0 or not_low_enough_r != 0) and (not_high_enough_l != 0 and not_high_enough_r != 0):
+            message = "You need to work on your form, try to lower your arms more and raise them higher."
+        elif not_low_enough_l != 0 or not_low_enough_r != 0:
+            message = "You need to work on your form, try to lower your arms more." 
+        else:
+            message = "You need tro work on your form, try to raise your arms higher."   
+        
+        return mark, message
+    
+    def checkLateralRaises(self, left_shoulder_angles, right_shoulder_angles):
+        filtered_left_angles = savgol_filter(left_shoulder_angles, window_length=20, polyorder=2, mode= "nearest")
+        filtered_right_angles = savgol_filter(right_shoulder_angles, window_length=20, polyorder=2,  mode= "nearest")
+
+        max_left_id = find_peaks(filtered_left_angles)[0]
+        max_right_id = find_peaks(filtered_right_angles)[0]
+
+        min_left_id = find_peaks(-filtered_left_angles)[0]
+        min_right_id = find_peaks(-filtered_right_angles)[0]
+
+
+        # plt.figure(figsize=(12, 6))
+        # plt.plot(left_shoulder_angles, label='Unghi Dreapta Brut', alpha=0.7, color = 'blue')
+        # plt.plot(filtered_left_angles, label='Unghi Dreapta Prelucrat', alpha=0.7, color = 'red')
+        # # plt.plot(right_arm_angles, label='Unghi Dreapta Brut', alpha=0.7, color = 'blue')
+        # # plt.plot(filtered_right_angles, label='Unghi Dreapta Prelucrat', alpha=0.7, color = 'red')
+        # plt.show()
+
+
+        not_low_enough_l = 0
+        not_high_enough_l = 0
+
+        not_low_enough_r = 0
+        not_high_enough_r = 0
+
+        mark_l = 0
+        mark_r = 0
+
+
+
+        if len(min_left_id) < len(max_left_id):
+            rep_capture_error = 1
+            index_number_l = len(min_left_id)
+        else:
+            rep_capture_error = 0
+            index_number_l = len(max_left_id)
+
+        for i in range(index_number_l):
+            if filtered_left_angles[max_left_id[i+ rep_capture_error]] < 80:
+                not_low_enough_l += 1
+            if filtered_left_angles[min_left_id[i ]] > 30:
+                not_high_enough_l += 1
+
+        number_of_mistakes_l = not_high_enough_l + not_low_enough_l
+        mark_l = 10 - 5 * number_of_mistakes_l / index_number_l
+
+
+
+
+
+        if len(min_right_id) <= len(max_right_id):
+            rep_capture_error = 0
+            index_number_r = len(min_right_id)
+        else:
+            rep_capture_error = 1
+            index_number_r = len(max_right_id)
+
+        for i in range(index_number_r):
+            if filtered_right_angles[max_right_id[i]] < 80:
+                not_low_enough_r += 1
+            if filtered_right_angles[min_right_id[i + rep_capture_error]] > 30:
+                not_high_enough_r += 1
+        number_of_mistakes_r = not_high_enough_r + not_low_enough_r
+        mark_r = 10 - 5 * number_of_mistakes_r / index_number_r
+
+
+        mark = (mark_r+mark_l)/2
+        
+        if not_low_enough_l == 0 and not_high_enough_l == 0 and not_low_enough_r == 0 and not_high_enough_r == 0:
+            message = "Perfect form!"
+        elif (not_low_enough_l != 0 or not_low_enough_r != 0) and (not_high_enough_l != 0 and not_high_enough_r != 0):
+            message = "You need to work on your form, try to lower your arms more and raise them higher."
+        elif not_low_enough_l != 0 or not_low_enough_r != 0:
+            message = "You need to work on your form, try to lower your arms more." 
+        else:
+            message = "You need tro work on your form, try to raise your arms higher."
+        print(not_high_enough_l, not_high_enough_r, not_low_enough_l, not_low_enough_r)   
+        
+        return mark, message
+    
     def getRightArm(self):
 
         right_shoulder = self.lmlist[12][1:3]
@@ -155,4 +326,16 @@ class ExerciseAnalyzer():
         left_arm_angle = self.findAngle(left_shoulder, left_elbow, left_wrist)
         return left_arm_angle
 
-        
+    def getLeftShoulder(self):
+        left_shoulder = self.lmlist[11][1:3]
+        left_elbow = self.lmlist[13][1:3]
+        left_hip = self.lmlist[23][1:3]
+        left_shoulder_angle = self.findAngle(left_elbow,left_shoulder,  left_hip)
+        return left_shoulder_angle
+    
+    def getRightShoulder(self):
+        right_shoulder = self.lmlist[12][1:3]
+        right_elbow = self.lmlist[14][1:3]
+        right_hip = self.lmlist[24][1:3]
+        right_shoulder_angle = self.findAngle(right_elbow,right_shoulder,  right_hip)
+        return right_shoulder_angle
